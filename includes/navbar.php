c57e0075411807e728a82286ce223c6b8d25899a
DilanDFrya <dilandilman.work@gmail.com>
2026-04-06 19:45:47 +0300

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #17a2b8;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Logo" width="30"></a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Register</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">🛒 Cart <sup>1</sup></a></li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="bg-secondary text-white p-2 ps-4">
+    <small>Welcome Guest | <a href="#" class="text-white text-decoration-none">Login</a></small>
+</div>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+
+<div class="container-fluid mt-3">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="text-center my-4">
+                <h1>E-Commerce Website</h1>
+                <p>Communication is at the heart of E-commerce and community</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="images/laptop.png" class="card-img-top" alt="Product">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title.</p>
+                            <a href="#" class="btn btn-info text-white">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="images/laptop2.png" class="card-img-top" alt="Product">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title.</p>
+                            <a href="#" class="btn btn-info text-white">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="images/phone.png" class="card-img-top" alt="Product">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title.</p>
+                            <a href="#" class="btn btn-info text-white">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-2 bg-info p-0 text-center text-white">
+            <h5 class="p-3 m-0">Delivery Brands</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item bg-secondary text-white border-0">Brand 1</li>
+                <li class="list-group-item bg-secondary text-white border-0">Brand 2</li>
+            </ul>
+            <h5 class="p-3 m-0">Categories</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item bg-secondary text-white border-0">Category 1</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php // include('includes/footer.php'); ?>
+</body>
+
+</html>
